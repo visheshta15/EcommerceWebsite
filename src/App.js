63bf7e964cec2b17056bbf79454d357b29f4a54a
visheshta kushwaha <visheshta@wonderpillars.in>
2021-12-20 02:57:21 +0530

    add card component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import {data} from './component/data'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    
+          {console.log(data.products)}
+        <div className="grid-container">
+          <header className="row">
+              <div>
+                  <a className='brand' href="/">amazona</a>
+              </div>
+              <div>
+                  <a href="/cart">Cart</a>
+                  <a href="/signin">Sign In</a></div>
+          </header>
+          <main>
+              <div className="row center">
+                {data.products.map((product)=>(
+                  <div key={product._id} className="card">
+                    <a href={`/product/${product._id}`}>
+                        <img className='medium'  src={product.image} alt="product" />
+                    </a>   
+                    <div className="card-body">
+                        <a href={`/product/${product._id}`}>
+                        <h2>{product.brand}</h2>
+                        <p>{product.name}</p>
+                        </a>
+                        <div className="rating">
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="far fa-star"></i></span>
+                            <span><i className="fa fa-star-half-alt"></i></span>
+                            <span><i className="fa fa-star-o"></i></span>
+                        </div>
+                        <div className="price">{`₹${product.price}`}</div>
+                    </div>
+                  </div>
+
+                )
+                )}
+                  
+              </div>
+          </main>
+          <footer className="row center">
+            All right reserved
+          </footer>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/component/data.js
@@ -0,0 +1,138 @@
+const shirt1 = "https://image.freepik.com/free-photo/portrait-happy-cute-girl-with-pink-hair-tattooed-hand-flattered-by-compliment-laughs-touches-cheek-standing-wearing-white-t-shirt-denim-jacket_295783-1876.jpg"
+const shirt2 = "https://image.freepik.com/free-photo/elegant-ginger-girl-making-funny-faces-pink-background-stunning-curly-woman-denim-jacket-looking-camera_197531-17413.jpg"
+const shirt3 = "https://image.freepik.com/free-photo/with-cheerful-carefree-rebellious-attitude-joking-sticking-tongue-out-having-fun_1194-89827.jpg"
+
+const top1 = "https://image.freepik.com/free-photo/charming-lady-jeans-dancing-pink-background-cute-curly-girl-fashionable-clothes-red-high-heels-laughs-isolated-backdrop_197531-18604.jpg"
+const top2 = "https://image.freepik.com/free-photo/portrait-woman-full-growth-wearing-white-t-shirt-jeans_197531-16971.jpg"
+const top3  = "https://image.freepik.com/free-photo/full-lenght-portrait-woman-elegant-linen-top-with-balloon-sleeves-blue-jeans-posing-pink_273443-4432.jpg"
+const top4 = "https://image.freepik.com/free-photo/full-lenght-portrait-woman-elegant-linen-top-with-balloon-sleeves-blue-jeans-posing-pink_273443-4432.jpg"
+
+const pant1 = "https://image.freepik.com/free-photo/crop-hipster-couple-kissing-street_211139-1485.jpg"
+const pant2 = "https://image.freepik.com/free-photo/carefree-woman-vintage-jeans-looking-front_197531-16434.jpg"
+const pant3 = "https://image.freepik.com/free-photo/fashion-short-jean-pants-women_41471-8791.jpg"
+
+export const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: shirt1,
+            price: 1500,
+            brand: 'Nike',
+            rating: 3,
+            numReviews: 10,
+            description: 'high quality product'
+            
+        },
+        {
+            _id: '2',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: shirt2,
+            price: 1800,
+            brand: 'Nike',
+            rating: 3,
+            numReviews: 10,
+            description: 'high quality product'
+            
+        },
+        {
+            _id: '3',
+            name: 'Adidas Denim Shirt',
+            category: 'Shirts',
+            image: shirt3,
+            price: 2000,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 30,
+            description: 'high quality product'
+            
+        },
+        {
+            _id: '4',
+            name: 'Regular top',
+            category: 'Tops',
+            image: top1,
+            price: 1500,
+            brand: 'Mango',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+            
+        },
+        {
+            _id: '5',
+            name: 'Smart Casual top',
+            category: 'Tops',
+            image: top2,
+            price: 1000,
+            brand: 'Mango',
+            rating: 3,
+            numReviews: 26,
+            description: 'high quality product'
+            
+        },
+        {
+            _id: '6',
+            name: 'Shirt style top',
+            category: 'Tops',
+            image: top3,
+            price: 1500,
+            brand: 'Only',
+            rating: 4,
+            numReviews: 12,
+            description: 'high quality product'
+            
+        },
+        {
+            _id: '7',
+            name: 'Solid top',
+            category: 'Tops',
+            image: top4,
+            price: 1500,
+            brand: 'Lewis',
+            rating: 2.5,
+            numReviews: 50,
+            description: 'high quality product'
+            
+        },
+        {
+            _id: '8',
+            name: 'Solid lounge pants ',
+            category: 'Pants',
+            image: pant1,
+            price: 2000,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+            
+        },
+        {
+            _id: '9',
+            name: 'Blue pants ',
+            category: 'Pants',
+            image: pant2,
+            price: 1020,
+            brand: 'Adidas',
+            rating: 3,
+            numReviews: 29,
+            description: 'high quality product'
+            
+        },
+        {
+            _id: '10',
+            name: 'Blue shorts',
+            category: 'Pants',
+            image: pant3,
+            price: 2500,
+            brand: 'Roadster',
+            rating: 3,
+            numReviews: 29,
+            description: 'high quality product'
+            
+        },
+        
+    ]
+}
